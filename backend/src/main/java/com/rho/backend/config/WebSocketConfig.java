0c616eda39c1a3b2a1dd2e4ea19e7240ecaf9c0a
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-06-20 14:28:11 +0800

    feat: implemented a task scheduler for the heartbeat

--- backend/src/main/java/com/rho/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/rho/backend/config/WebSocketConfig.java
@@ -3,8 +3,11 @@ package com.rho.backend.config;
 import com.rho.backend.handler.JwtHandshakeHandler;
 import com.rho.backend.service.JwtHandshakeInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,9 +27,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         // Enables memory-based broker for /topic (broadcast) and /queue (private)
         config.enableSimpleBroker("/topic", "/queue")
-                .setHeartbeatValue(new long[]{10000, 10000});
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(heartBeatTaskScheduler());
         config.setApplicationDestinationPrefixes("/app"); // client -> server
         config.setUserDestinationPrefix("/user"); // server specific user
+    }
+
+    @Bean
+    public TaskScheduler heartBeatTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 
     @Override
